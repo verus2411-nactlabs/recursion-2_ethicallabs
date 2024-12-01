a9baad8875f4db154a4feabf5144943fe51334da
unknown <lehonglong2411@gmail.com>
2024-12-01 17:07:40 +0700

    new version

--- recursion#2/main.cpp
+++ recursion#2/main.cpp
@@ -10,24 +10,23 @@
 using namespace std;
 int T, n;
 
-bool checkPrime( int numberCheck, int checkValue){
-    if (numberCheck == checkValue) {
+bool checkPrime( int numberCheck, int checkValue = 2){
+    // base case
+    if ( numberCheck <= 2)
+        return numberCheck == 2 ? true : false;
+    if( numberCheck % checkValue == 0 )
+        return false;
+    if( checkValue * checkValue > numberCheck ){\
         return true;
     }
-    if (numberCheck % checkValue == 0) {
-        return false;
-    }
-    else{
-        
-        checkPrime( numberCheck, checkValue + 1 );
-    }
-    
-    return true;
+
+    // recursion
+    checkPrime( numberCheck, checkValue + 1);
 }
 
 int main(int argc, const char * argv[]) {
     ios::sync_with_stdio();
-//    freopen("sample_input.txt", "r", stdin);
+   freopen("sample_input.txt", "r", stdin);
     cin >> T;
     
     for( int test_no = 1; test_no <= T; test_no++){
